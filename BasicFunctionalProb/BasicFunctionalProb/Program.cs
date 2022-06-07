812ce3f1af09615d151d8818eb01c145555fd4c0
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-06-07 14:45:02 +0530

    [Shivaraj] Added Power of two problem

--- BasicFunctionalProb/BasicFunctionalProb/Program.cs
+++ BasicFunctionalProb/BasicFunctionalProb/Program.cs
@@ -11,18 +11,25 @@ namespace BasicFunctionalProb
                Console.WriteLine("Welocme to Basic Functional Programming Problems");
                Console.WriteLine("1 for Flip coin and print percentage of Heads and tails");
                Console.WriteLine("2 for Leap Year");
+               Console.WriteLine("3 for Power of two");
 
-               int Problem = Convert.ToInt32(Console.ReadLine());
+            int Problem = Convert.ToInt32(Console.ReadLine());
                switch (Problem)
                {
                 case 1:
-                        FilpCoinProb flipcoinprob = new FilpCoinProb();
-                        flipcoinprob.Flip_Percentage();
+                    FilpCoinProb flipcoinprob = new FilpCoinProb();
+                    flipcoinprob.Flip_Percentage();
                     break;
                 case 2:
                     LeapYearProb leapyearprob = new LeapYearProb();
                     leapyearprob.LeapYear();
                     break;
+                case 3:
+                    PowerOfTwo poweroftwo = new PowerOfTwo();
+                    poweroftwo.Power2();
+                    break;
+
+
 
                }
            }
